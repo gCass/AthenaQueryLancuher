eee99e1ec514ad36e25cf7b623db0591ecea3502
gCass <g.cassini227@gmail.com>
2023-02-01 12:32:22 +0100

    Rimossa vecchia query

--- test_query/query_prova.sql
+++
@@ -1,9 +0,0 @@
-create table l3_rep_research.prove_query_launcher_gc_temp
-WITH ( format = 'PARQUET',
-       parquet_compression = 'SNAPPY',
-      external_location = 's3://axa-it-dl-nonprod-dev-l3-reporting-eu-central-1/l3_rep_research/prove_query_launcher_gc_temp')
-as 
-
-select 1 as A, now() as B
-from l2_portafoglio.polizza
-limit 10
